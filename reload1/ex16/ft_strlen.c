32c98ea64a51c98133276001537cf2ef88186ff1
nfiamma <noahfiamma11@gmail.com>
2025-09-25 22:29:21 +0200

    ex16

---
+++ reload1/ex16/ft_strlen.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfiamma <nfiamma@student.42barcelona.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 21:15:40 by nfiamma           #+#    #+#             */
+/*   Updated: 2025/09/25 22:28:17 by nfiamma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <unistd.h>
+//#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		i++;
+	}
+	return(i);
+}
+
+/* int	main(void)
+{
+	int i;
+	char str[9] = "School42";
+
+	i = ft_strlen(str);
+	printf("The strng %s is %d characters long\n", str, i);
+	return (0);
+} */
