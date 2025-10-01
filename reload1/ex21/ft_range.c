56d21a2dd2d6485a7335ac2ab2e42377827a2663
Noah Fiamma <nfiamma@car9s2.42barcelona.com>
2025-10-01 18:26:22 +0200

    ex21

---
+++ reload1/ex21/ft_range.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfiamma <nfiamma@student.42barcelona.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 17:51:52 by nfiamma           #+#    #+#             */
+/*   Updated: 2025/10/01 18:22:18 by nfiamma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*array;
+	int	i;
+
+	i = 0;
+	array = (int *) malloc(sizeof(int) * ((max - min) + 1));
+	if (min >= max)
+	{
+		array = NULL;
+		return (0);
+	}
+	while (min < max)
+	{
+		array[i] = min;
+		i++;
+		min++;
+	}
+	return (array);
+}
+
+int	main(void)
+/* {
+	int	min = -10;
+	int	max = 10;
+	int	i = 0;
+	int	*range = ft_range(min, max);
+	int	size = max - min;
+
+	while (i < size)
+	{
+		printf("%d\n", range[i]);
+		i++;
+	}
+	free(range);
+} */
