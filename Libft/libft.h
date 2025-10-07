d5b0955be4b7582bdc309ad1992781e4b88efdf4
Noah Fiamma <nfiamma@car9s4.42barcelona.com>
2025-10-07 19:08:08 +0200

    header y makefile

---
+++ Libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfiamma <nfiamma@student.42barcelona.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 19:00:58 by nfiamma           #+#    #+#             */
+/*   Updated: 2025/10/07 19:05:26 by nfiamma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <stddef.h>
+# include <limits.h>
+
+/*(Prototipos de funciones abajo:)*/
+
+#endif
