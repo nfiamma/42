3c71108b7b88556d7e7eb1860785ed5e8a3dd028
nfiamma <noahfiamma11@gmail.com>
2025-10-16 18:17:02 +0200

    hasta strrchr

---
+++ Libft/ft_strchr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfiamma <nfiamma@student.42barcelona.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 17:34:13 by nfiamma           #+#    #+#             */
+/*   Updated: 2025/10/16 17:34:21 by nfiamma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
